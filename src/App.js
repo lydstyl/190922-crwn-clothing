15dc13ecd359917943528a4783c30cd3fc5dd764
Gabriel Brun <lydstyl@gmail.com>
2019-09-23 15:45:46 +0200

    Header with links

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
