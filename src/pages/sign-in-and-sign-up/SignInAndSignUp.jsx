ad61a5a3ba579c7c41e015f3a5f04fa1f87c53a5
Gabriel Brun <lydstyl@gmail.com>
2019-09-23 23:44:34 +0200

    firebase store user in db

--- src/pages/sign-in-and-sign-up/SignInAndSignUp.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUp.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
+
+import './signInAndSignUp.scss';
 
 const SignInAndSignUp = () => {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
