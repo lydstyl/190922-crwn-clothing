e5a8737d144a580f3a4981b618960de2a0c5cfec
Gabriel Brun <lydstyl@gmail.com>
2019-09-24 14:43:21 +0200

    mapDispatchToProps

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -7,7 +8,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.scss';
 
-export default function Header({ currentUser }) {
+const Header = ({ currentUser }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -32,4 +33,13 @@ export default function Header({ currentUser }) {
       </div>
     </div>
   );
-}
+};
+
+const maptStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(
+  maptStateToProps,
+  null
+)(Header);
