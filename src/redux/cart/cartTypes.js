f594255fc1c4dceeb606938165bfdd651f2f1fdf
Gabriel Brun <lydstyl@gmail.com>
2019-10-03 16:43:17 +0200

    addItem & removeItem to Checkout page

--- src/redux/cart/cartTypes.js
+++ src/redux/cart/cartTypes.js
@@ -1,5 +1,10 @@
 export const CartActionTypes = {};
 
-['TOGGLE_CART_HIDDEN', 'ADD_ITEM'].forEach(action => {
+[
+  'TOGGLE_CART_HIDDEN',
+  'ADD_ITEM',
+  'CLEAR_ITEM_FROM_CART',
+  'REMOVE_ITEM'
+].forEach(action => {
   CartActionTypes[action] = action;
 });
