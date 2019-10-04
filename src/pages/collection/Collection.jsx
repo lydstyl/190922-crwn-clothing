cacc5972de19f754796eb155e79edbd0594682f5
Gabriel Brun <lydstyl@gmail.com>
2019-10-04 13:46:30 +0200

    Collection routing & selector

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/CollectionItem';
 
+import { selectCollection } from '../../redux/shop/shopSelector';
+
 import './collection.scss';
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ collection }) => {
+  const { items } = collection;
+
   return (
     <div className='collection-page'>
       <h2>CATEGORY PAGE</h2>
-      <div>{match.params.collectionId}</div>
+
+      <div className='preview'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state) // this selector needs a part of the state depending on the URL parameter
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
-import CategoryPage from '../collection/Collection';
+import CollectionPage from '../collection/Collection';
 
 const ShopPage = ({ match }) => {
   // match, location and history are there because of <Route path='/shop' component={ShopPage} /> in App.js
@@ -11,7 +11,7 @@ const ShopPage = ({ match }) => {
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
       <Route
         path={`${match.path}/:collectionId`}
-        component={CategoryPage}
+        component={CollectionPage}
       />{' '}
       {/* We add a param named categoryId in the match object in the Category page */}
     </div>
