845f5b863828fe7f2c5d0443c2164159c2476831
Gabriel Brun <lydstyl@gmail.com>
2019-10-05 21:10:33 +0200

    import shop data from firebase

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -4,18 +4,36 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 import CollectionPage from '../collection/Collection';
 
-const ShopPage = ({ match }) => {
-  // match, location and history are there because of <Route path='/shop' component={ShopPage} /> in App.js
-  return (
-    <div>
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route
-        path={`${match.path}/:collectionId`}
-        component={CollectionPage}
-      />{' '}
-      {/* We add a param named categoryId in the match object in the Category page */}
-    </div>
-  );
-};
+import {
+  firestore,
+  convertCollectionSnapshotToMap
+} from '../../firebase/firebase.utils';
+import { async } from 'q';
+
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+
+    collectionRef.onSnapshot(async snapshot =>
+      convertCollectionSnapshotToMap(snapshot)
+    );
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />{' '}
+        {/* We add a param named categoryId in the match object in the Category page */}
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
